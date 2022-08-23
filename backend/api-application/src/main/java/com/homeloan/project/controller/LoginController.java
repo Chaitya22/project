69f485fd5d973a5ff86fbfb9f29edb0500305f39
Chaitya Shah <chaityashah22@gmail.com>
2022-08-23 13:56:39 +0530

    Added basic controller and connected app to mysql

---
+++ backend/api-application/src/main/java/com/homeloan/project/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.homeloan.project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+public class LoginController {
+	
+	@GetMapping("login")
+	public String login() {
+		return "Logged in succesfully";
+	}
+	
+}
